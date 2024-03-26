002ac9ff2d70f11246fdb1cdc434b20783a36caa
TardigradeTyrs <morrison.kendrick7@gmail.com>
2024-03-26 10:14:41 -0600

    yes

--- http-server/server.js
+++ http-server/server.js
@@ -2,7 +2,7 @@ http = require('http')
 let HOST = "localhost"
 let PORT = 3000
 const server = http.createServer((request, response) => {
-  response.end(`Hello, Galvanize! Server is running on ${HOST}:${PORT}`)
+  response.end(`Hello, Galvanize!`)
 })
 server.listen(3000, "localhost", () => {
   console.log(`Server is running at ${HOST};${PORT}`)
